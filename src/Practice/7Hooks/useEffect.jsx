9eae4d7d2d0e19969ec9c560b66ab809f8e3df5f
King-Hard <208336213+King-Hard@users.noreply.github.com>
2025-06-15 19:34:22 +0800

    .

---
+++ src/Practice/7Hooks/useEffect.jsx
@@ -0,0 +1,39 @@
+import {useEffect, useRef, useState} from "react"
+
+const Effect = () =>{
+    const [todos, setTodos] = useState([])
+    const [count, setCount] = useState(0)
+    const [message, setMessage] = useState("")
+
+    useEffect(() =>{
+        setMessage(`Updated: ${count}`)
+    }, [count])
+
+
+    useEffect(() =>{
+        fetch("https://jsonplaceholder.typicode.com/todos")
+            .then(response => response.json())
+            .then(data => setTodos(data))
+    }, [])
+    
+    return(
+        <>
+            <div>
+                <p>Count : {count}</p>
+                <p>{message}</p>
+                <button className="border p-5" onClick={() => setCount(count + 1)}>Increment</button>
+            </div>
+
+            <div>
+                <ul>
+                    {todos.map(todo => (
+                        <li key={todo.id}>{todo.title}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Effect
+
